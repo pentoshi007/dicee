66fd766ae253984159fb962818a3ac503ab5e53e
Aniket Pandey <76784594+pentoshi007@users.noreply.github.com>
2022-12-06 15:06:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// var randomNumber1 = Math.ceil((Math.random()) * 6);
+// var randomNumber2 = Math.ceil((Math.random()) * 6);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "⛳️Player 1 won";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "⛳️Player 2 won";
+
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
